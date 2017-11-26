84d1edfc560328fd7d6f4b414d1d4ed39823ce0f
Aniket <andy12290@gmail.com>
2017-11-25 20:09:48 -0500

    Added new files

---
+++ Doc.py
@@ -0,0 +1,8 @@
+# Docstring we have to defined in the associating with documnetaion
+# Line should start with Capital letter and end with perios
+def my_fuction():
+    """ Demonstarate the Docstring."""
+    return None
+#print ("using__doc__:")
+print(my_fuction.__doc__)
+# we can access the doc string to define the function and classess
--- Logical.py
+++ Logical.py
@@ -6,3 +6,14 @@ line = "Geek1 \nGeek2 \nGeek3";
 # splitting string by delimeter
 print(line.split()) # normal splitting
 print(line.split(' ',1))
+
+
+# Formatting
+variable = '15'
+s = ('variable are string= %s' %variable)
+print(s)
+
+# we will convert and again will file
+variable = int(variable)
+s = ('variable are string= %d' %variable)
+print(s)
